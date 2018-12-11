bbde58e2aecc0d01e9c3bc65b5721f6f8f355a90
Matthew Fung <mattfung94@gmail.com>
2018-12-11 16:44:33 +0000

    Add divider

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './tesco-modern.css';
 import './components/atoms/spacings.css';
 import './fonts.css';
 import './colours.css';
+import Image from './components/organisms/ddl-hero/hero.jpg';
 // Components
 import Nav from './components/organisms/ddl-nav/ddl-nav';
 import Hero from './components/organisms/ddl-hero/ddl-hero';
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <main className="ddl-wrapper">
         <Nav txt="Navigation"/>
+        <div className="ddl-hero_img" style={{backgroundImage: `url(${Image})`}}></div>
         <Hero />
         <Nav txt="Footer"/>
       </main>
---
+++ src/components/atoms/divider/divider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ddl-divider.css';
+const Divider = (props) => {
+  return (
+    <div className="ddl-divider mb-32"></div>
+  )
+}
+
+export default Divider;
