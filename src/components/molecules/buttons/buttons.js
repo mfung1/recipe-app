12e52c0e72172fe680e569fa8a64f5ba03f66650
Matthew Fung <mattfung94@gmail.com>
2018-12-11 10:17:49 +0000

    Add button component

---
+++ src/components/molecules/buttons/buttons.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TheIcon from '../../atoms/icons/icon';
+import './ddl-btn.css';
+const Button = (props) => {
+  let modifier = `ddl-btn-`;
+  props.filled
+  ?
+  modifier += `flld`
+  :
+  modifier += `hllw`
+  return (
+    <a href="https://www.tesco.com" className={`ddl-btn ${modifier} mr-16 mb-24`}>
+      <span class="ddl-lbl ddl-t2">{props.txt}</span>
+      <TheIcon icon="chevron" color={props.filled ? 'wht' : 'blu'}/>
+    </a>
+  )
+}
+
+export default Button;
