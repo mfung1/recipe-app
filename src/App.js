4e1b43ca5e39a9b85e7e64556dfc1de650a419d8
Matthew Fung <mattfung94@gmail.com>
2018-12-11 10:17:18 +0000

    Basic App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './tesco-modern.css';
+import './components/atoms/spacings.css';
+import './fonts.css';
+import './colours.css';
+// Components
+import Nav from './components/organisms/ddl-nav/ddl-nav';
+import Hero from './components/organisms/ddl-hero/ddl-hero';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main className="ddl-wrapper">
+        <Nav txt="Navigation"/>
+        <Hero />
+        <Nav txt="Footer"/>
+      </main>
     );
   }
 }
